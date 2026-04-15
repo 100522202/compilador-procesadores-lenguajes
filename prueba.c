5a47db45c4734ffb2b7930b2071640b1a7a08208
100522202 <100522202@alumnos.uc3m.es>
2026-04-15 12:27:12 +0200

    Pruebas de compilación

--- prueba.c
+++ prueba.c
@@ -1,9 +1,8 @@
-// Prueba basica 2 de printf 
 #include <stdio.h>
-
+ 
 main () 
 {
-	printf ("El texto no se imprime %d %s %d", 123+1, " el texto se imprime ", 124+1) ;
+	puts (" El texto se imprime ") ;
 }
 
 //@ (main)
